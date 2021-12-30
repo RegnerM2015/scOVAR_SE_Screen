360f9ba03770dda76d17aa30aaf43ce4dd05a12c
Matt Regner <regnerm@live.unc.edu>
2021-12-30 16:42:12 -0500

    Update Motif_Analysis_SE60_SE14.sh

--- Motif_Analysis_SE60_SE14.sh
+++ Motif_Analysis_SE60_SE14.sh
@@ -13,9 +13,9 @@ export PATH=/share/apps/install-compute/bin/bcftools:$PATH
 dir=/datastore/nextgenout5/share/labs/francolab/Data/refdata-cellranger-atac-GRCh38-1.2.0/fasta
 
 # Get sequence of enhancers for SE60
-bedtools getfasta -fi ${dir}/genome.fa -fo enhancer_1_SE60.fa -bed enhancer_1_SE60.bed
-bedtools getfasta -fi ${dir}/genome.fa -fo enhancer_2_SE60.fa -bed enhancer_2_SE60.bed
-bedtools getfasta -fi ${dir}/genome.fa -fo enhancer_3_SE60.fa -bed enhancer_3_SE60.bed
+bedtools getfasta -fi ${dir}/genome.fa -fo enhancer_1_SE60.fa -bed Enhancer_1_SE60.bed
+bedtools getfasta -fi ${dir}/genome.fa -fo enhancer_2_SE60.fa -bed Enhancer_2_SE60.bed
+bedtools getfasta -fi ${dir}/genome.fa -fo enhancer_3_SE60.fa -bed Enhancer_3_SE60.bed
 
 # Run FIMO motif scanning
 fimo --bgfile motif-file --oc enhancer_1_SE60_fimo ./JASPAR2020_CORE_vertebrates_non-redundant_pfms_meme.meme enhancer_1_SE60.fa
@@ -24,9 +24,9 @@ fimo --bgfile motif-file --oc enhancer_3_SE60_fimo ./JASPAR2020_CORE_vertebrates
 
 
 # Get sequence of enhancers for SE14
-bedtools getfasta -fi ${dir}/genome.fa -fo enhancer_1_SE14.fa -bed enhancer_1_SE14.bed
-bedtools getfasta -fi ${dir}/genome.fa -fo enhancer_2_SE14.fa -bed enhancer_2_SE14.bed
-bedtools getfasta -fi ${dir}/genome.fa -fo enhancer_3_SE14.fa -bed enhancer_3_SE14.bed
+bedtools getfasta -fi ${dir}/genome.fa -fo enhancer_1_SE14.fa -bed Enhancer_1_SE14.bed
+bedtools getfasta -fi ${dir}/genome.fa -fo enhancer_2_SE14.fa -bed Enhancer_2_SE14.bed
+bedtools getfasta -fi ${dir}/genome.fa -fo enhancer_3_SE14.fa -bed Enhancer_3_SE14.bed
 
 # Run FIMO motif scanning
 fimo --bgfile motif-file --oc enhancer_1_SE14_fimo ./JASPAR2020_CORE_vertebrates_non-redundant_pfms_meme.meme enhancer_1_SE14.fa
