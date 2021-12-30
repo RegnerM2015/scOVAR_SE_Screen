7bcb1df3abeadd2eb3d2a0df3fe877914d5511f3
Matt Regner <regnerm@live.unc.edu>
2021-12-30 16:43:28 -0500

    Update FIMO_TF_rank.R

--- FIMO_TF_rank.R
+++ FIMO_TF_rank.R
@@ -1,7 +1,22 @@
+########################
+# Matt Regner
+# March-December 2021
+# Process Motif Results
+########################
+
+library(Seurat)
+library(ArchR)
+library(ggplot2)
+library(stringr)
+library(dplyr)
+
 ovar_HGSOC_scRNA_processed <- readRDS("./ovar_HGSOC_scRNA_processed.rds")
 
+malignant <- c("0-Epithelial cell","2-Epithelial cell",
+"3-Epithelial cell","7-Epithelial cell",
+"11-Epithelial cell","16-Epithelial cell")
 
-hgsoc <-subset(ovar_HGSOC_scRNA_processed,cell.type == "0-Epithelial cell")
+hgsoc <-subset(ovar_HGSOC_scRNA_processed,cell.type %in% malignant)
 
 counts <- hgsoc@assays$RNA@data
 counts.bulk <- as.data.frame(rowSums(counts))
@@ -10,7 +25,7 @@ counts.bulk$gene <- rownames(counts.bulk)
 
 
 
-fimo_outs <- list.files(pattern = "SE60_fimo")
+fimo_outs <- list.dirs()[grep("SE60_fimo",list.dirs())]
 
 
 for (i in fimo_outs){
@@ -24,19 +39,23 @@ for (i in fimo_outs){
   
   test <- dplyr::filter(test,V9 <= 0.1)
   test <- dplyr::arrange(test,desc(Expr))
-  write.table(test,paste0(i,"_w_expr.txt"),sep = "\t",quote = F,row.names = F,col.names = F)
+  write.table(test,paste0(i,"_w_expr-Update.txt"),sep = "\t",quote = F,row.names = F,col.names = F)
 }
 
-
-VlnPlot(ovar_HGSOC_scRNA_processed,features = c("SOX4","ATF4","SOX4","YY1","ATF4","TEAD1"),
-        ncol = 6,pt.size = 0,idents = "0-Epithelial cell",same.y.lims = T)+
-  ggsave("Enhancer_1_2_3_vln_SE60.pdf",width = 8,height = 4)
+hgsoc$Cancer <- ifelse(hgsoc$cell.type %in% malignant,"Cancer","Non-Cancer")
+Idents(hgsoc) <- "Cancer"
+VlnPlot(hgsoc,features = c("ATF4","SOX4","ELF3","YY1","ATF4","JUN"),
+        ncol = 6,pt.size = 0,idents = "Cancer",same.y.lims = T)+
+  ggsave("Enhancer_1_2_3_vln_SE60-Update.pdf",width = 8,height = 4)
 
 
 ovar_HGSOC_scRNA_processed <- readRDS("./ovar_HGSOC_scRNA_processed.rds")
 
+malignant <- c("0-Epithelial cell","2-Epithelial cell",
+               "3-Epithelial cell","7-Epithelial cell",
+               "11-Epithelial cell","16-Epithelial cell")
 
-hgsoc <-subset(ovar_HGSOC_scRNA_processed,cell.type == "11-Epithelial cell")
+hgsoc <-subset(ovar_HGSOC_scRNA_processed,cell.type %in% malignant)
 
 counts <- hgsoc@assays$RNA@data
 counts.bulk <- as.data.frame(rowSums(counts))
@@ -45,7 +64,7 @@ counts.bulk$gene <- rownames(counts.bulk)
 
 
 
-fimo_outs <- list.files(pattern = "SE14_fimo")
+fimo_outs <- list.dirs()[grep("SE14_fimo",list.dirs())]
 
 
 for (i in fimo_outs){
@@ -59,12 +78,14 @@ for (i in fimo_outs){
   
   test <- dplyr::filter(test,V9 <= 0.1)
   test <- dplyr::arrange(test,desc(Expr))
-  write.table(test,paste0(i,"_w_expr.txt"),sep = "\t",quote = F,row.names = F,col.names = F)
+  write.table(test,paste0(i,"_w_expr-Update.txt"),sep = "\t",quote = F,row.names = F,col.names = F)
 }
 
+hgsoc$Cancer <- ifelse(hgsoc$cell.type %in% malignant,"Cancer","Non-Cancer")
+Idents(hgsoc) <- "Cancer"
+VlnPlot(hgsoc,features = c("ELF3","KLF5","JUND","JUNB","KLF6","ELF3"),
+        ncol = 6,pt.size = 0,idents = "Cancer",same.y.lims = T)+
+  ggsave("Enhancer_1_2_3_vln_SE14-Update.pdf",width = 8,height = 4)
 
-VlnPlot(ovar_HGSOC_scRNA_processed,features = c("KLF6","YY1","ELF3","KLF6","ELF3","KLF6"),
-        ncol = 6,pt.size = 0,idents = "11-Epithelial cell",same.y.lims = T)+
-  ggsave("Enhancer_1_2_3_vln_SE14.pdf",width = 8,height = 4)
 
-
+writeLines(capture.output(sessionInfo()), "sessionInfo-FIMO_TF_Rank.txt")
